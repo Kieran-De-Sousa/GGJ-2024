1c34aa401622947d780c752be2063aff97816b12
Oscar Wilkinson <Oscar2.Wilkinson@live.uwe.ac.uk>
2024-01-26 21:36:26 +0000

    Foundation of Cinemachine And Multiplayer

--- GGJ-2024/Assets/Scripts/Multiplayer/TestPlayerScript.cs
+++ GGJ-2024/Assets/Scripts/Multiplayer/TestPlayerScript.cs
@@ -2,19 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Cinemachine;
 
 public class TestPlayerScript : MonoBehaviour
 {
     public float move_speed;
     private Vector2 move_vec;
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        CinemachineTargetGroup targets = FindObjectOfType<CinemachineTargetGroup>();
+        for (int i = 0; i < 4; i++)
+        {
+            if (targets.m_Targets[i].target == null)
+            {
+                targets.m_Targets[i].target = transform;
+                break;
+            }
+        }
+    }
 
     public void Move(InputAction.CallbackContext info)
     {
         move_vec = info.ReadValue<Vector2>();
     }
 
-    public void FixedUpdate()
+    public void Update()
     {
-        transform.position += new Vector3(move_vec.x, 0, move_vec.y) * move_speed * Time.deltaTime;
+        rb.velocity = new Vector3(move_vec.x * move_speed, Mathf.Clamp(rb.velocity.y, -5, 5), move_vec.y * move_speed);
     }
 }
