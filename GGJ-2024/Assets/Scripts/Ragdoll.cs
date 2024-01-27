534331cd2b46638fa634f8d3e3dbf8323e373319
Kieran De Sousa <kieran2.desousa@live.uwe.ac.uk>
2024-01-27 16:28:48 +0000

    Add Get Back Up Animation

--- GGJ-2024/Assets/Scripts/Ragdoll.cs
+++ GGJ-2024/Assets/Scripts/Ragdoll.cs
@@ -8,8 +8,13 @@ public class Ragdoll : MonoBehaviour
     [SerializeField] Rigidbody myRigidbody;
     [SerializeField] GameObject rootObject;
     [SerializeField] float respawnTime = 30f;
+
     Collider[] allColliders;
     List<Collider> childColliders;
+
+    public Animator playerAnimator;
+
+    private bool isRagdolling = false;
 
     void Awake()
     {
@@ -33,6 +38,7 @@ public class Ragdoll : MonoBehaviour
 
     public void ToggleRagdoll(bool bIsRagdoll)
     {
+        isRagdolling = bIsRagdoll;
         GetComponent<Animator>().enabled = !bIsRagdoll;
         myRigidbody.useGravity = !bIsRagdoll;
         myCollider.enabled = !bIsRagdoll;
@@ -52,7 +58,10 @@ public class Ragdoll : MonoBehaviour
     private IEnumerator GetBackUp()
     {
         yield return new WaitForSeconds(respawnTime);
+
         ToggleRagdoll(false);
+        playerAnimator.SetTrigger("GetUp");
+
         this.transform.position = rootObject.transform.position + new Vector3(0f, 0.5f, 0f);
     }
 
