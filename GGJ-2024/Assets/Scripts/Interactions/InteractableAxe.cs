17f8eac0aec0363ec7a16012878196688a8e58ee
Calvin Oktavialdy Setia <calvin2.setia@live.uwe.ac.uk>
2024-01-27 14:19:16 +0000

    Oscillating hammer & vertical movement

--- GGJ-2024/Assets/Scripts/Interactions/InteractableAxe.cs
+++ GGJ-2024/Assets/Scripts/Interactions/InteractableAxe.cs
@@ -5,23 +5,55 @@ using UnityEngine;
 
 public class InteractableAxe : Interactable
 {
-    public float rotationSpeed = 360;
+    [SerializeField] private float rotationSpeed = 360;
 
-    public float lifeSpanInSeconds = 1;
+    [SerializeField] private float lifeSpanInSeconds = 5;
+
+    [SerializeField] private Vector2 rotationEulerAnglesRange = new Vector2(0, 240);
+    [SerializeField] private float rotationEulerAngle;
+
+    [SerializeField] private float heightOnSpawn = 35;
+    [SerializeField] private float heightOnOscillation = 25;
+
+    public bool _stopping;
     private void Awake()
     {
-        Quaternion rotation = Quaternion.Euler(0, 0, 180);
+        Quaternion rotation = Quaternion.Euler(0, 0, rotationEulerAngle);
         transform.rotation = rotation;
+
+        transform.position = new Vector3(transform.position.x, heightOnSpawn, transform.position.z);
     }
 
     private void Update()
     {
+        if (transform.position.y > heightOnOscillation && !_stopping)
+        {
+            transform.position = new Vector3(transform.position.x,
+                transform.position.y - (heightOnSpawn - heightOnOscillation) * Time.deltaTime,
+                transform.position.z);
+        }
+
+        if (_stopping)
+        {
+            transform.position = new Vector3(transform.position.x,
+                transform.position.y + (heightOnSpawn - heightOnOscillation) * Time.deltaTime, 
+                transform.position.z);
+        }
+        
+        rotationEulerAngle = Mathf.SmoothStep(rotationEulerAnglesRange.x, rotationEulerAnglesRange.y,
+            Mathf.PingPong(Time.time * 1, 1));
+        
         Vector3 rotation = transform.rotation.eulerAngles;
         
         transform.rotation = Quaternion.Euler(rotation.x, rotation.y,
-            rotation.z - rotationSpeed * Time.deltaTime);
+            rotationEulerAngle);
 
         lifeSpanInSeconds -= 1 * Time.deltaTime;
+
+        if (lifeSpanInSeconds <= 1)
+        {
+            _stopping = true;
+        }
 
         if (lifeSpanInSeconds <= 0)
         {
